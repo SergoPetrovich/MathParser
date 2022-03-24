41441d7eab26ab710eeb67e826ddf6a864fd1474
SP <tk34moodle@tk34.mskobr.ru>
2022-03-24 11:28:37 +0300

    Добавьте файлы проекта.

---
+++ MathParser/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using org.mariuszgromada.math.mxparser;
+using Expression = org.mariuszgromada.math.mxparser.Expression;
+
+namespace MathParser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            Expression ex = new Expression(stro.Text);
+            stro.Text = ex.calculate().ToString();
+
+
+        }
+    }
+}
